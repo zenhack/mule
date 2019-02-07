8400d28c0191117a5b68a80fe6b8726f067957ec
Ian Denhardt <ian@zenhack.net>
2019-02-07 14:34:11 -0500

    Add a comment

--- lint.mli
+++ lint.mli
@@ -1,1 +1,9 @@
+(** Run several ad-hoc static checks on an expression:
+  *
+  * - make sure there are no expressions with duplicate record fields.
+  * - check for unbound variables.
+  *
+  * This is invoked by the type checker, before doing the more principled
+  * hindley-milner type inference algorithm.
+  *)
 val check : 'i Ast.Surface.Expr.t -> (Error.t, unit) OrErr.t
