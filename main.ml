b52f79a517498ca944d3e409bd8f2bcfb1010a8b
Ian Denhardt <ian@zenhack.net>
2019-02-06 23:49:56 -0500

    Start stubbing out logic for processing files.

--- main.ml
+++ main.ml
@@ -1,1 +1,9 @@
-let () = Repl.loop ()
+let () =
+  match Array.length Sys.argv with
+  | 1 -> Repl.loop ()
+  | len ->
+      for i = 1 to len do
+        let path = Array.get Sys.argv i in
+        (* TODO: actually process the files. *)
+        print_endline path
+      done
