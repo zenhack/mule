ba0eac04fb428b901a5554e5c9c124a6032310e8
Ian Denhardt <ian@zenhack.net>
2019-03-03 18:09:48 -0500

    Document gensym.
    The ordering constraint in particular is important to state, since we
    rely on this.

--- src/gensym.mli
+++ src/gensym.mli
@@ -1,4 +1,8 @@
 
+(* Generate a unique integer, greater than all previously generated
+ * integers. *)
 val gensym : unit -> int
 
+(* Generate an anonymous variable name -- guaranteed not to clash with
+ * any user-named variables, nor any other anonymous variables. *)
 val anon_var : unit -> Ast.Var.t
