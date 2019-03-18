fa5ebc6d3d7c0b82316905cef9fd2a721ad14df6
Ian Denhardt <ian@zenhack.net>
2019-03-18 00:58:35 -0400

    Extend label/var to be usable with base's maps/sets.

--- src/ast/common_ast.ml
+++ src/ast/common_ast.ml
@@ -1,24 +1,33 @@
 open Base
 
-module StringWrapper : sig
-  type t
+module type StringWrapper_S = sig
+  include Comparator.S
+
+  val compare : t -> t -> int
+  val sexp_of_t : t -> Sexp.t
 
   val of_string : string -> t
   val to_string : t -> string
 
-  val compare : t -> t -> int
-
   val equal : t -> t -> bool
-end = struct
-  type t = string
+end
+
+module StringWrapper : StringWrapper_S = struct
+  module T = struct
+    type t = string
+    let sexp_of_t = sexp_of_string
+    let compare = String.compare
+  end
+
+  include T
+  include Comparator.Make(T)
 
   let of_string s = s
   let to_string s = s
 
-  let compare = String.compare
 
   let equal = String.equal
 end
 
-module Var : module type of StringWrapper = StringWrapper
-module Label : module type of StringWrapper = StringWrapper
+module Var : StringWrapper_S = StringWrapper
+module Label : StringWrapper_S = StringWrapper
