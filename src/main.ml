235718f438d4ac0dcd549c634b1bf825759b0c0e
Ian Denhardt <ian@zenhack.net>
2019-03-18 11:18:39 -0400

    Move main over to base.

--- src/main.ml
+++ src/main.ml
@@ -1,13 +1,17 @@
+open Base
+
+module In_chan = Stdio.In_channel
+
 let () =
   match Array.length Sys.argv with
   | 1 -> Repl.loop ()
   | len ->
       for i = 1 to len - 1 do
         let path = Array.get Sys.argv i in
-        let fd = open_in path in
-        let str = really_input_string fd (in_channel_length fd) in
-        close_in fd;
+        let fd = In_chan.create path in
+        let str = In_chan.input_all fd in
+        In_chan.close fd;
         match Run.run str with
           | Ok () -> ()
-          | Err () -> exit 1
+          | Err () -> Caml.exit 1
       done
