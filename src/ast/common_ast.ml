cc8cd6a54c44561631a387116595ce7513d220a2
Ian Denhardt <ian@zenhack.net>
2019-03-18 01:00:27 -0400

    Change a few names.

--- src/ast/common_ast.ml
+++ src/ast/common_ast.ml
@@ -1,33 +1,35 @@
 open Base
 
-module type StringWrapper_S = sig
-  include Comparator.S
+module Name = struct
+  module type S = sig
+    include Comparator.S
 
-  val compare : t -> t -> int
-  val sexp_of_t : t -> Sexp.t
+    val compare : t -> t -> int
+    val sexp_of_t : t -> Sexp.t
 
-  val of_string : string -> t
-  val to_string : t -> string
+    val of_string : string -> t
+    val to_string : t -> string
 
-  val equal : t -> t -> bool
-end
-
-module StringWrapper : StringWrapper_S = struct
-  module T = struct
-    type t = string
-    let sexp_of_t = sexp_of_string
-    let compare = String.compare
+    val equal : t -> t -> bool
   end
 
-  include T
-  include Comparator.Make(T)
+  module Impl : S = struct
+    module T = struct
+      type t = string
+      let sexp_of_t = sexp_of_string
+      let compare = String.compare
+    end
 
-  let of_string s = s
-  let to_string s = s
+    include T
+    include Comparator.Make(T)
+
+    let of_string s = s
+    let to_string s = s
 
 
-  let equal = String.equal
+    let equal = String.equal
+  end
 end
 
-module Var : StringWrapper_S = StringWrapper
-module Label : StringWrapper_S = StringWrapper
+module Var : Name.S = Name.Impl
+module Label : Name.S = Name.Impl
