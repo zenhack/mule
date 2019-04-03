9610c0fa9b6a97f998e25e70877664f22fc9741b
Ian Denhardt <ian@zenhack.net>
2019-04-02 20:52:28 -0400

    Start breaking the type checker up into multiple files.

--- src/typecheck.ml
+++ src/typecheck.ml
@@ -1,38 +1,6 @@
 open Ast.Desugared
 open Gensym
-
-(* The type of values associated with unification variables *)
-type u_type =
-  [ `Free of tyvar
-  | `Fn of (tyvar * u_type UnionFind.var * u_type UnionFind.var)
-  | `Record of (tyvar * u_row UnionFind.var)
-  | `Union of (tyvar * u_row UnionFind.var)
-  ]
-and u_row =
-  [ `Free of tyvar
-  | `Extend of (tyvar * Ast.Label.t * u_type UnionFind.var * u_row UnionFind.var)
-  | `Empty of tyvar
-  ]
-and bound_ty = [ `Rigid | `Flex ]
-and 'a bound =
-  { b_ty: bound_ty
-  ; b_at: 'a
-  }
-and tyvar =
-  { ty_id: int
-  ; ty_bound: bound_target bound UnionFind.var
-  }
-and g_node =
-  { g_id: int
-  ; g_bound: (g_node bound) option
-  ; g_child: u_type UnionFind.var Lazy.t
-  }
-and bound_target =
-  [ `Ty of u_type UnionFind.var Lazy.t
-  | `G of g_node
-  ]
-
-type permission = F | R | L
+open Typecheck_types
 
 let perm_eq: permission -> permission -> bool = Poly.equal
 
