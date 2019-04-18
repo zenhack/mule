8ee9b2ded9ba573c957213ce01ab0bb8ea70cbab
Ian Denhardt <ian@zenhack.net>
2019-04-17 20:02:30 -0400

    Add a lint check for unbound type vars.

--- src/lint.ml
+++ src/lint.ml
@@ -1,104 +1,126 @@
-open Ast
+open Ast.Surface
 open Ast.Surface.Expr
-open Result.Monad_infix
 
-let (>>) x y = x >>= fun _ -> y
+let rec collect_pat_vars = function
+  | Pattern.Wild -> VarSet.empty
+  | Pattern.Var v -> VarSet.singleton v
+  | Pattern.Ctor(_, p) -> collect_pat_vars p
+  | Pattern.Annotated(p, _) -> collect_pat_vars p
 
-(* Free variables in an expression *)
-let rec free_vars env =
-  function
-  | Var v ->
-      if Set.mem env v then
-        Set.empty (module Var)
-      else
-        Set.singleton (module Var) v
-  | Lam ((pat :: pats), body) ->
-      case_free_vars env (pat, (Lam (pats, body)))
-  | Lam ([], body) ->
-      free_vars env body
-  | App (f, x) ->
-      Set.union (free_vars env f) (free_vars env x)
-  | Record fields -> fields_free_vars env fields
-  | Update(e, fields) ->
-      Set.union
-        (free_vars env e)
-        (fields_free_vars env fields)
-  | GetField(e, _) ->
-      free_vars env e
-  | Ctor _ ->
-      Set.empty (module Var)
-  | Match (e, cases) ->
-      List.fold
-        ~f:Set.union
-        ~init:(free_vars env e)
-        (List.map cases ~f:(case_free_vars env))
-  | Let (pat, e, body) ->
-      Set.union
-        (case_free_vars env (pat, e))
-        (case_free_vars env (pat, body))
-and case_free_vars env (p, body) =
-  match p with
-    | Ast.Surface.Pattern.Wild ->
-        free_vars env body
-    | Ast.Surface.Pattern.Var v ->
-        free_vars (Set.add env v) body
-    | Ast.Surface.Pattern.Ctor (_, p') ->
-        case_free_vars env (p', body)
-    | Ast.Surface.Pattern.Annotated (p', _) ->
-        case_free_vars env (p', body)
-and fields_free_vars env fields =
-  fields
-    |> List.map ~f:(fun (_, _, v) -> free_vars env v)
-    |> List.fold ~f:Set.union ~init:(Set.empty (module Var))
+let unboundVar v =
+  raise (MuleErr.MuleExn (MuleErr.UnboundVar v))
 
 (* Check for unbound variables. *)
 let check_unbound_vars expr =
-  let free = free_vars (Set.empty (module Var)) expr in
-  match Set.find free ~f:(fun _ -> true) with
-  | Some x -> Error (MuleErr.UnboundVar x)
-  | None -> Ok ()
+  let rec go_expr typ term = function
+    | Var v when Set.mem term v -> ()
+    | Var v -> unboundVar v
+    | Lam([], body) ->
+        go_expr typ term body
+    | Lam(pat :: pats, body) ->
+        go_pat typ pat;
+        let term_new = collect_pat_vars pat in
+        go_expr typ (Set.union term term_new) (Lam(pats, body))
+    | App(f, x) -> go_expr typ term f; go_expr typ term x
+    | Record fields ->
+        List.iter fields ~f:(go_field typ term)
+    | GetField (e, _) -> go_expr typ term e
+    | Ctor _ -> ()
+    | Update (e, fields) ->
+        go_expr typ term e;
+        List.iter fields ~f:(go_field typ term)
+    | Match (e, cases) ->
+        go_expr typ term e;
+        List.iter cases ~f:(go_case typ term)
+    | Let (pat, e, body) ->
+        go_pat typ pat;
+        go_expr typ term e;
+        let e_new = collect_pat_vars pat in
+        go_expr typ (Set.union term e_new) body
+  and go_field typ term (_, ty, e) =
+    go_expr typ term e;
+    Option.iter ty ~f:(go_type typ)
+  and go_case typ term (pat, e) =
+    go_pat typ pat;
+    let pat_new = collect_pat_vars pat in
+    go_expr typ (Set.union term pat_new) e
+  and go_pat typ = function
+    | Pattern.Wild -> ()
+    | Pattern.Var _ -> ()
+    | Pattern.Ctor(_, p) -> go_pat typ p
+    | Pattern.Annotated(_, ty) -> go_type typ ty
+  and go_type typ = function
+    | Type.Var v when Set.mem typ v -> ()
+    | Type.Var v -> unboundVar v
+    | Type.Quant(_, vars, ty) ->
+        go_type (List.fold ~init:typ ~f:Set.add vars) ty
+    | Type.Recur(var, ty) ->
+        go_type (Set.add typ var) ty
+    | Type.Fn(param, ret) ->
+        go_type typ param;
+        go_type typ ret
+    | Type.Record (Type.Field(_, ty) :: rest) ->
+        go_type typ ty;
+        go_type typ (Type.Record rest)
+    | Type.Record (Type.Rest var :: rest) ->
+        go_type typ (Type.Var var);
+        go_type typ (Type.Record rest)
+    | Type.Record [] -> ()
+    | Type.Ctor _ -> ()
+    | Type.App (f, x) ->
+        go_type typ f;
+        go_type typ x
+    | Type.Union(x, y) ->
+        go_type typ x;
+        go_type typ y
+    | Type.RowRest v ->
+        go_type typ (Type.Var v)
+  in
+  go_expr VarSet.empty VarSet.empty expr
 
 (* Check for duplicate record fields *)
 let check_duplicate_record_fields =
   let rec go =
     let rec check_fields all dups = function
       | (x, _, v) :: xs ->
-          go v >>
+          go v;
           if Set.mem all x then
             check_fields all (Set.add dups x) xs
           else
             check_fields (Set.add all x) dups xs
+      | [] when Set.is_empty dups -> ()
       | [] ->
-          if Set.is_empty dups then
-            Ok ()
-          else
-            Error
-              ( MuleErr.DuplicateFields (Set.to_list dups)
-              )
+          raise (MuleErr.MuleExn (MuleErr.DuplicateFields (Set.to_list dups)))
     in
     let rec check_cases = function
-      | [] -> Ok ()
-      | ((_, body) :: cs) -> go body >> check_cases cs
+      | [] -> ()
+      | ((_, body) :: cs) -> go body; check_cases cs
     in
     function
     | Record fields ->
         check_fields LabelSet.empty LabelSet.empty fields
     | Update(e, fields) ->
-        go e
-        >> check_fields LabelSet.empty LabelSet.empty fields
+        go e;
+        check_fields LabelSet.empty LabelSet.empty fields
 
     (* The rest of this is just walking down the tree *)
     | Lam (_, body) -> go body
     | Match(e, cases) ->
-        go e >> check_cases cases
-    | App (f, x) -> go f >> go x
+        go e; check_cases cases
+    | App (f, x) -> go f; go x
     | GetField(e, _) -> go e
-    | Let (_, e, body) -> go e >> go body
+    | Let (_, e, body) -> go e; go body
 
-    | Var _ -> Ok ()
-    | Ctor _ -> Ok ()
+    | Var _ -> ()
+    | Ctor _ -> ()
   in go
 
 let check expr =
-  check_unbound_vars expr
-  >> check_duplicate_record_fields expr
+  try
+    begin
+      check_unbound_vars expr;
+      check_duplicate_record_fields expr;
+      Ok ()
+    end
+  with
+    MuleErr.MuleExn e -> Error e
