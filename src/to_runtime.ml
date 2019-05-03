2a0ef218482280d4aeadd77e173a408ae29a3538
Ian Denhardt <ian@zenhack.net>
2019-05-03 05:29:49 -0400

    First stab at evaluation for recursive let.
    I'm sure it's broken. In particular, we're not attaching the environment
    to the bound variables (just in the body).

--- src/to_runtime.ml
+++ src/to_runtime.ml
@@ -56,8 +56,36 @@ let rec translate: int -> int VarMap.t -> D.t -> (int * R.t) =
       )
   | D.Let (v, e, body) ->
       translate depth env (D.App (D.Lam (v, body), e))
-  | D.LetRec _ ->
-      failwith "TODO"
+  | D.LetRec (bindings, body) ->
+      let len = List.length bindings in
+      let env' = List.foldi bindings
+        ~init:env
+        ~f:(fun i env (var, _) ->
+          Map.set env ~key:var ~data:(depth + 1 + i)
+        )
+      in
+      let depth' = depth + len in
+      let binds = List.map
+        bindings
+        ~f:(fun (_, e) ->
+          let (ncaps, e') = translate depth' env' e in
+          (ncaps, R.Lazy (ref e')))
+      in
+      let binds =
+        List.map binds ~f:snd
+        |> List.rev
+      in
+      let (ncaps, body') =
+        translate (depth' + 1) env' body
+      in
+      let ncaps = max 0 (ncaps - len) in
+      ( ncaps
+      (* We pass the bound variables in as an unused parameter, which has
+       * the effect of forcing them before the body is evaluated. They are
+       * also embedded in the body's environment, for actual use sites
+       *)
+      , R.App(R.Lam(ncaps, binds, body'), Vec(Array.of_list binds))
+      )
 
 let translate: D.t -> R.t =
   fun exp -> snd (translate 0 VarMap.empty exp)
