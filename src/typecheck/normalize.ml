a8c569e3f4e1c71c3e3d9f1e15dadfd96d9ec1a5
Ian Denhardt <ian@zenhack.net>
2019-05-23 20:59:10 -0400

    Add a note about possible fixes.

--- src/typecheck/normalize.ml
+++ src/typecheck/normalize.ml
@@ -10,6 +10,9 @@ let rec whnf: u_type UnionFind.var -> u_type UnionFind.var =
          * should be done with the bounds here; right now we're just retaining
          * the outermost bound, but I need to think more carefully about what
          * the right thing is.
+         *
+         * It would at least be sound to raise the two bounds to meet, but I
+         * don't know immediately what that would do to completeness.
          *)
         begin match UnionFind.get arg1 with
         | `Quant(_v2, arg2) ->
