f5476f995ad67d695b7a24b29f7208d5db3b856b
Ian Denhardt <ian@zenhack.net>
2019-05-29 21:04:53 -0400

    Fix indentation

--- src/typecheck/normalize.ml
+++ src/typecheck/normalize.ml
@@ -8,18 +8,18 @@ let whnf: u_type UnionFind.var -> u_type UnionFind.var =
   fun uvar -> uvar
 
 let rec pair: u_var -> u_var -> (u_var * u_var) =
-    fun l r ->
-      let l, r = whnf l, whnf r in
-      match UnionFind.get l, UnionFind.get r with
-      | `Quant _, `Quant _ -> (l, r)
-      | `Quant _, t ->
-        let tv' =
-          { ty_id = Gensym.gensym ()
-          ; ty_bound = ref (!((get_tyvar t).ty_bound))
-          }
-        in
-        (l, UnionFind.make (`Quant(tv', r)))
-      | _, `Quant _ ->
-        let (r, l) = pair r l in
-        (l, r)
-      | _ -> (l, r)
+  fun l r ->
+    let l, r = whnf l, whnf r in
+    match UnionFind.get l, UnionFind.get r with
+    | `Quant _, `Quant _ -> (l, r)
+    | `Quant _, t ->
+      let tv' =
+        { ty_id = Gensym.gensym ()
+        ; ty_bound = ref (!((get_tyvar t).ty_bound))
+        }
+      in
+      (l, UnionFind.make (`Quant(tv', r)))
+    | _, `Quant _ ->
+      let (r, l) = pair r l in
+      (l, r)
+    | _ -> (l, r)
