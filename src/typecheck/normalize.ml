7107f94b3881499c9d38d510da557e139b257c0f
Ian Denhardt <ian@zenhack.net>
2019-05-29 21:03:31 -0400

    Don't coalesce quant nodes.
    We don't need to for correctness, and per the (now deleted) comment, I
    wasn't sure we were handling bounds correctly. Just skipping this step
    makes it easier to see that it's correct.

--- src/typecheck/normalize.ml
+++ src/typecheck/normalize.ml
@@ -1,26 +1,11 @@
 open Typecheck_types
 
 (* Reduce the contents of the unification variable to weak head normal form. *)
-let rec whnf: u_type UnionFind.var -> u_type UnionFind.var =
-  fun uvar -> match UnionFind.get uvar with
-    (* Free vars and constant constructors are already in whnf: *)
-    | `Free _ | `Const _ -> uvar
-    | `Quant(v1, arg1) ->
-      (* Adjacent quantifiers should be merged. XXX: not 100% sure what
-       * should be done with the bounds here; right now we're just retaining
-       * the outermost bound, but I need to think more carefully about what
-       * the right thing is.
-       *
-       * It would at least be sound to raise the two bounds to meet, but I
-       * don't know immediately what that would do to completeness.
-      *)
-      begin match UnionFind.get arg1 with
-        | `Quant(_v2, arg2) ->
-          UnionFind.merge (fun _ _ ->`Quant(v1, arg2)) uvar arg1;
-          whnf uvar
-        | _ ->
-          uvar
-      end
+let whnf: u_type UnionFind.var -> u_type UnionFind.var =
+  (* Right now this is a no-op, but once we add higher-kinds/type operators,
+   * we'll do beta reduction here.
+   *)
+  fun uvar -> uvar
 
 let rec pair: u_var -> u_var -> (u_var * u_var) =
     fun l r ->
