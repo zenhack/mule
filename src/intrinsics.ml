2dc79f9e9da9ec161b3e0b299d890a048b82d2d8
Ian Denhardt <ian@zenhack.net>
2019-06-26 20:55:42 -0400

    Add some text related intrinsics.

--- src/intrinsics.ml
+++ src/intrinsics.ml
@@ -15,6 +15,10 @@ let assert_int = function
   | R.Expr.Integer n -> n
   | _ -> failwith "BUG: run-time type error."
 
+let assert_text = function
+  | R.Expr.Text s -> s
+  | _ -> failwith "BUG: run-time type error."
+
 let int_binop f =
   ( fn_t int_t (fn_t int_t int_t)
   , prim (fun x -> prim (fun y -> R.Expr.Integer (f (assert_int x) (assert_int y))))
@@ -24,12 +28,49 @@ let dict kvs =
   List.map kvs ~f:(fun (k, v) -> (Var.of_string k, v))
   |> Map.of_alist_exn (module Var)
 
+let row kvs =
+  ( ()
+  , List.map kvs ~f:(fun (k, v) -> (Label.of_string k, v))
+  , None
+  )
+
+let recordType tys vals =
+  D.Type.Record
+    { r_info = ()
+    ; r_types = row tys
+    ; r_values = row vals
+    }
+
+let recordVal kvs =
+  R.Expr.Record (
+    List.map kvs ~f:(fun (k, v) -> (Label.of_string k, v))
+    |> Map.of_alist_exn (module Label)
+  )
+
 let values = dict
   [ "add", int_binop Z.add
   ; "sub", int_binop Z.sub
   ; "mul", int_binop Z.mul
   ; "div", int_binop Z.div
   ; "rem", int_binop Z.rem
+  ; "text",
+      ( recordType
+        [ "t", text_t ]
+        [ "append", fn_t text_t (fn_t text_t text_t)
+        ; "from-int", fn_t int_t text_t
+        ; "length", fn_t text_t int_t
+        ]
+      , recordVal
+        [ "append",
+            prim (fun l ->
+              prim (fun r ->
+                R.Expr.Text (assert_text l ^ assert_text r)))
+        ; "from-int",
+            prim (fun x -> R.Expr.Text (Z.to_string (assert_int x)))
+        ; "length",
+            prim (fun s -> R.Expr.Integer (Z.of_int (String.length (assert_text s))))
+        ]
+      )
   ]
 
 let types = dict
