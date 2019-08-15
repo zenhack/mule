ef613cf7229f65f35c2a2cb6b8ac1cb160ba72cd
Ian Denhardt <ian@zenhack.net>
2019-08-15 19:15:02 -0400

    desugared ast: Hand-written sexp_of_t.
    This is *far* more readable.

--- src/ast/desugared_ast.ml
+++ src/ast/desugared_ast.ml
@@ -19,7 +19,10 @@ end
 
 module Type = struct
   type quantifier = [ `All | `Exist ]
-  [@@deriving sexp_of]
+
+  let sexp_of_quantifier = function
+    | `All -> Sexp.Atom "all"
+    | `Exist -> Sexp.Atom "exist"
 
   type 'i t =
     | Fn of ('i * Var.t option * 'i t * 'i t)
@@ -37,10 +40,50 @@ module Type = struct
     | Opaque of 'i
     | TypeLam of ('i * Var.t * 'i t)
     | App of ('i * 'i t * 'i t)
-  [@@deriving sexp_of]
   and 'i row =
     ('i * (Label.t * 'i t) list * Var.t option)
-  [@@deriving sexp_of]
+
+  let rec sexp_of_t: 'i t -> Sexp.t = function
+    | Fn(_, None, param, ret) ->
+        Sexp.List [sexp_of_t param; Sexp.Atom "->"; sexp_of_t ret]
+    | Fn(_, Some v, param, ret) ->
+        Sexp.List
+          [ Sexp.List [ Var.sexp_of_t v; Sexp.Atom ":"; sexp_of_t param ]
+          ; Sexp.Atom "->"
+          ; sexp_of_t ret
+          ]
+    | Recur(_, v, body) ->
+        Sexp.List [Sexp.Atom "rec"; Var.sexp_of_t v; sexp_of_t body]
+    | Var(_, v) ->
+        Var.sexp_of_t v
+    | Path(_, v, ls) -> Sexp.(
+        List ([Atom "."; Var.sexp_of_t v] @ List.map ls ~f:Label.sexp_of_t)
+      )
+    | Record { r_info = _; r_types; r_values } -> Sexp.(
+        List
+          [ Atom "record"
+          ; List [Atom "types"; sexp_of_row r_types]
+          ; List [Atom "values"; sexp_of_row r_values]
+          ]
+      )
+    | Union row ->
+        Sexp.(List [Atom "union"; sexp_of_row row])
+    | Quant (_, q, v, t) ->
+        Sexp.List [sexp_of_quantifier q; Sexp.Atom(Var.to_string v); sexp_of_t t]
+    | TypeLam(_, v, body) ->
+        Sexp.List [Sexp.Atom "lam"; Sexp.Atom(Var.to_string v); sexp_of_t body]
+    | Named(_, name) -> Sexp.Atom name
+    | Opaque _ -> Sexp.Atom "<opaque>"
+    | App(_, f, x) -> Sexp.List [sexp_of_t f; sexp_of_t x]
+  and sexp_of_row (_, fields, rest) =
+    let fields =
+      List.map fields ~f:(fun (l, t) ->
+          Sexp.List [Label.sexp_of_t l; sexp_of_t t]
+        )
+    in
+    match rest with
+    | Some v -> Sexp.List (fields @ [Sexp.Atom ("..." ^ Var.to_string v)])
+    | None -> Sexp.List fields
 
   let get_info = function
     | Fn(x, _, _, _) -> x
@@ -111,7 +154,68 @@ module Expr = struct
     | Let of (Var.t * 'i t * 'i t)
     | LetType of ((Var.t * 'i Type.t) list * 'i t)
     | Const of Const.t
-  [@@deriving sexp_of]
+
+  let rec sexp_of_t = function
+    | Var v -> Sexp.Atom (Var.to_string v)
+    | Lam(v, body) ->
+        Sexp.List [Sexp.Atom "fn"; Var.sexp_of_t v; sexp_of_t body]
+    | App(f, x) ->
+        Sexp.List [sexp_of_t f; sexp_of_t x]
+    | Fix `Let -> Sexp.Atom "fix/let"
+    | Fix `Record -> Sexp.Atom "fix/record"
+    | EmptyRecord -> Sexp.Atom "{}"
+    | GetField(_, l) -> Sexp.List [Sexp.Atom "."; Label.sexp_of_t l]
+    | Update(`Value, l) -> Sexp.List [Sexp.Atom ".="; Label.sexp_of_t l]
+    | Update(`Type, l) -> Sexp.List [Sexp.Atom ".type="; Label.sexp_of_t l]
+    | Ctor(l, arg) -> Sexp.List [Label.sexp_of_t l; sexp_of_t arg]
+    | Match {cases; default} ->
+        let cs =
+          [ Sexp.Atom "match"
+          ; Map.sexp_of_m__t
+              (module Label)
+              (fun (v, e) ->
+                 Sexp.List [Var.sexp_of_t v; sexp_of_t e]
+              )
+              cases
+          ]
+        in
+        begin match default with
+          | None -> Sexp.List cs
+          | Some (maybe_v, def) ->
+              let v = match maybe_v with
+                | Some v -> Var.sexp_of_t v
+                | None -> Sexp.Atom "_"
+              in
+              Sexp.List (cs @ [Sexp.List [v; sexp_of_t def]])
+        end
+    | ConstMatch {cm_cases; cm_default} ->
+        Sexp.List
+          [ Sexp.Atom "match/const"
+          ; Map.sexp_of_m__t (module Const) sexp_of_t cm_cases
+          ; Sexp.List [Sexp.Atom "_"; sexp_of_t cm_default]
+          ]
+    | WithType ty ->
+        Sexp.List [Sexp.Atom ":"; Type.sexp_of_t ty]
+    | Witness ty ->
+        Sexp.List [Sexp.Atom "type"; Type.sexp_of_t ty]
+    | Let(v, e, body) ->
+        Sexp.List
+          [ Sexp.Atom "let"
+          ; Sexp.List [Var.sexp_of_t v; sexp_of_t e]
+          ; sexp_of_t body
+          ]
+    | LetType(binds, body) ->
+        Sexp.List
+          [ Sexp.Atom "let-type"
+          ; Sexp.List
+              (List.map binds ~f:(fun (v, ty) ->
+                  Sexp.List [Var.sexp_of_t v; Type.sexp_of_t ty]
+                )
+              )
+          ; sexp_of_t body
+          ]
+    | Const c ->
+        Const.sexp_of_t c
 
   let apply_to_kids e ~f = match e with
     | Lam (v, body) -> Lam (v, f body)
--- src/pretty.ml
+++ src/pretty.ml
@@ -1,6 +1,6 @@
 open Ast
 open Ast.Desugared
 
-let typ t = Sexp.to_string_hum (Type.sexp_of_t (fun _ -> sexp_of_unit ()) t)
-let expr e = Sexp.to_string_hum (Expr.sexp_of_t (fun _ -> sexp_of_unit ()) e)
+let typ t = Sexp.to_string_hum (Type.sexp_of_t t)
+let expr e = Sexp.to_string_hum (Expr.sexp_of_t e)
 let runtime_expr e = Sexp.to_string_hum (Runtime.Expr.sexp_of_t e)
