7014e8851947a236cb479fe6201389f2a1537630
Ian Denhardt <ian@zenhack.net>
2019-10-03 12:24:07 -0400

    Expose fewer things from muleErr.

--- src/muleErr.mli
+++ src/muleErr.mli
@@ -1,11 +1,5 @@
 include module type of MuleErr_t
 
-val show_ctor: ctor -> string
-val show_op: op -> string
-val show_kind: kind -> string
-val show_type_error: (Types.reason * type_error) -> string
-val show_path_error: path_error -> string
 val show: t -> string
-
 val throw: t -> 'a
 val bug : string -> 'a
