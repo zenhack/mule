12dac859bfb7c343463ca85f074c414c68d1a8ba
Ian Denhardt <ian@zenhack.net>
2019-10-08 15:48:56 -0400

    Fix some formatting in the js runtime.

--- src/libmule/js_runtime.ml
+++ src/libmule/js_runtime.ml
@@ -1,31 +1,31 @@
 
 let src = "'use strict';
 const $update = (r, lbl, v) => {
-	var ret = {};
-	for(var k in Object.getOwnPropertyNames(r)) {
-		ret[k] = r[k];
-	}
-	ret[lbl] = v;
-	return ret;
+  var ret = {};
+  for(var k in Object.getOwnPropertyNames(r)) {
+    ret[k] = r[k];
+  }
+  ret[lbl] = v;
+  return ret;
 }
 
 const $lazy = (f) => {
-	return {state: 'delayed', thunk: f}
+  return {state: 'delayed', thunk: f}
 }
 
 const $force = (l, k) => {
-	switch(l.state) {
-	case 'ready':
-			return () => k(l.thunk);
-	case 'in-progress':
-			throw 'Infinite loop';
-	case 'delayed':
-			l.state = 'in-progress';
-      return l.thunk((val) => {
-        l.thunk = val;
-        l.state = 'ready';
-        return k(l.thunk);
-      })
+  switch(l.state) {
+  case 'ready':
+    return () => k(l.thunk);
+  case 'in-progress':
+    throw 'Infinite loop';
+  case 'delayed':
+    l.state = 'in-progress';
+    return l.thunk((val) => {
+      l.thunk = val;
+      l.state = 'ready';
+      return k(l.thunk);
+    })
   }
 }
 "
