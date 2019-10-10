aebf7e7b33f2ea5116fd4c74f349e4aa565d226c
Ian Denhardt <ian@zenhack.net>
2019-10-10 12:53:59 -0400

    Actually use cmdliner interface.

--- src/libmule/main.ml
+++ src/libmule/main.ml
@@ -43,77 +43,64 @@ let load_and_typecheck typ file_name =
             Caml.exit 1
       end
 
+let interp_cmd = function
+  | `Repl -> Repl.loop ()
+  | `Test path ->
+      let%lwt contents = Util.IO.slurp_file path in
+      let%lwt _ = Run.run contents in
+      Lwt.return ()
+  | `Build_js file_name ->
+        begin try%lwt
+          let%lwt dexp =
+            load_and_typecheck
+              Surface_ast.Type.(
+                (* For now we're not imposing any particular type,
+                 * so we just set it as `exists t. t`.
+                *)
+                let v = Var.of_string "t" in
+                Quant {
+                  q_quant = `Exist;
+                  q_vars = [v];
+                  q_body = Var {v_var = v};
+                }
+              )
+              file_name
+          in
+          let out = Lwt_io.stdout in
+          let%lwt _ = Lwt_io.write out Js_runtime.src in
+          To_js.translate_expr dexp
+          |> Js_pre.cps (fun x -> x)
+          |> Js_pre.to_js
+          |> Js_ast.expr
+          |> Fmt.(fun x -> x ^ s "\n")
+          |> Fmt.to_string
+          |> Lwt_io.write out
+        with
+        | Invalid_argument _ ->
+            let%lwt _ = Lwt_io.write Lwt_io.stderr "not enough arguments to build-js\n" in
+            Caml.exit 1
+        | e ->
+            raise e
+      end
+  | `Run targ ->
+      let runner_name = targ#runner in
+      let file_name = targ#file in
+      begin match Map.find Runners.by_name runner_name with
+        | None ->
+            let%lwt _ = Lwt_io.write Lwt_io.stderr "no such runner\n" in
+            Caml.exit 1
+        | Some runner ->
+            let%lwt dexp = load_and_typecheck runner.Runners.want_type file_name in
+            let rt_expr = To_runtime.translate dexp in
+            let%lwt _ = runner.Runners.run rt_expr in
+            Lwt.return ()
+      end
+
 let main () =
-  if Array.length Sys.argv <= 1 then
-    Repl.loop ()
-  else
-    begin match Array.get Sys.argv 1 with
-      | "repl" ->
-          Repl.loop ()
-      | "test" ->
-          each_file ~f:(fun input ->
-              let%lwt _ = Run.run input in
-              Lwt.return ()
-            )
-      | "build-js" ->
-          begin try%lwt
-              let file_name = Array.get Sys.argv 2 in
-              let%lwt dexp =
-                load_and_typecheck
-                  Surface_ast.Type.(
-                    (* For now we're not imposing any particular type,
-                     * so we just set it as `exists t. t`.
-                    *)
-                    let v = Var.of_string "t" in
-                    Quant {
-                      q_quant = `Exist;
-                      q_vars = [v];
-                      q_body = Var {v_var = v};
-                    }
-                  )
-                  file_name
-              in
-              let out = Lwt_io.stdout in
-              let%lwt _ = Lwt_io.write out Js_runtime.src in
-              To_js.translate_expr dexp
-              |> Js_pre.cps (fun x -> x)
-              |> Js_pre.to_js
-              |> Js_ast.expr
-              |> Fmt.(fun x -> x ^ s "\n")
-              |> Fmt.to_string
-              |> Lwt_io.write out
-            with
-            | Invalid_argument _ ->
-                let%lwt _ = Lwt_io.write Lwt_io.stderr "not enough arguments to build-js\n" in
-                Caml.exit 1
-            | e ->
-                raise e
-          end
-      | "run" ->
-          begin try%lwt
-              let runner_name = Array.get Sys.argv 2 in
-              let file_name = Array.get Sys.argv 3 in
-              begin match Map.find Runners.by_name runner_name with
-                | None ->
-                    let%lwt _ = Lwt_io.write Lwt_io.stderr "no such runner\n" in
-                    Caml.exit 1
-                | Some runner ->
-                    let%lwt dexp = load_and_typecheck runner.Runners.want_type file_name in
-                    let rt_expr = To_runtime.translate dexp in
-                    let%lwt _ = runner.Runners.run rt_expr in
-                    Lwt.return ()
-              end
-            with
-            | Invalid_argument _ ->
-                let%lwt _ = Lwt_io.write Lwt_io.stderr "not enough arguments to run\n" in
-                Caml.exit 1
-            | e ->
-                raise e
-          end
-      | cmd ->
-          let%lwt _ = Lwt_io.write Lwt_io.stderr ("Invalid command: " ^ cmd ^ "\n") in
-          Caml.exit 1
-    end
+  match Cli.parse_cmd () with
+  | `Ok cmd -> interp_cmd cmd
+  | `Version | `Help -> Caml.exit 0
+  | `Error _ -> Caml.exit 1
 
 let main () =
   Lwt_main.run (main ())
