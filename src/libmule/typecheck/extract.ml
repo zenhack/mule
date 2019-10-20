09fa9c0792c6ac05b157026fada240682f0f6256
Ian Denhardt <ian@zenhack.net>
2019-10-20 01:33:05 -0400

    Extract: expose the row version.
    This will be useful for debugging.

--- src/libmule/typecheck/extract.ml
+++ src/libmule/typecheck/extract.ml
@@ -8,115 +8,117 @@ let get_ty_id: u_type -> int = function
   | `Const(ty_id, _, _, _) -> ty_id
   | `Quant(ty_id, _, _, _, _) -> ty_id
 
+let var_of_int n =
+  Common_ast.Var.of_string ("$$" ^ Int.to_string n)
+let mk_var id =
+    ( DT.Var {
+        v_info = id;
+        v_var = var_of_int id;
+      }
+    , IntSet.singleton id
+    )
+
+let rec go seen uv =
+  let t = UnionFind.get uv in
+  let ty_id = get_ty_id t in
+  if Set.mem seen ty_id then
+    mk_var ty_id
+  else
+    begin match t with
+      | `Free _ -> mk_var ty_id
+      | `Quant(q_id, q, v_id, _, body) ->
+          let (body', fvs) = go (Set.add seen q_id) body in
+          maybe_add_recur
+            q_id
+            (Set.remove fvs v_id)
+            (DT.Quant {
+                q_info = q_id;
+                q_quant = q;
+                q_var = var_of_int v_id;
+                q_body = body';
+            })
+      | `Const(ty_id, `Named "{...}", [rtypes, _; rvals, _], _) ->
+          let seen' = Set.add seen ty_id in
+          let (r_types, fv_types) = go_row seen' rtypes in
+          let (r_values, fv_values) = go_row seen' rvals in
+          DT.Record {
+            r_info = ty_id;
+            r_types;
+            r_values;
+            r_src = ST.Record {r_items = []};
+          }
+          |> maybe_add_recur ty_id (Set.union fv_types fv_values)
+      | `Const(ty_id, `Named "|", [row, _], _) ->
+          let seen' = Set.add seen ty_id in
+          let (r, fvs) = go_row seen' row in
+          DT.Union{u_row = r}
+          |> maybe_add_recur ty_id fvs
+      | `Const(ty_id, c, args, _) ->
+          let seen' = Set.add seen ty_id in
+          let args_fvs =
+            List.map args ~f:(fun (t, _) -> go seen' t)
+          in
+          let args = List.map args_fvs ~f:fst in
+          let fvs =
+            List.map args_fvs ~f:snd
+            |> Set.union_list (module Int)
+          in
+          maybe_add_recur ty_id fvs (make_const_type ty_id c args)
+    end
+and go_row seen uv =
+  let r = UnionFind.get uv in
+  let ty_id = get_ty_id r in
+  match r with
+    | `Free _ ->
+        ( (ty_id, [], Some (var_of_int ty_id))
+        , IntSet.singleton ty_id
+        )
+    | `Const(_, `Named "<empty>", _, _) ->
+        ((ty_id, [], None), IntSet.empty)
+    | `Const(_, `Extend lbl, [h, _; t, _], _) ->
+        let (h', hfv) = go seen h in
+        let ((_, fields, tail), tfv) = go_row seen t in
+        ( (ty_id, ((lbl, h') :: fields), tail)
+        , Set.union hfv tfv
+        )
+    | _ ->
+        MuleErr.bug "Invalid row"
+and maybe_add_recur id fvs ty =
+  if Set.mem fvs id then
+    ( DT.Recur {
+        mu_info = id;
+        mu_var = var_of_int id;
+        mu_body = ty;
+      }
+    , Set.remove fvs id
+    )
+  else
+    (ty, fvs)
+and make_const_type id c args =
+  match c, args with
+  | `Named name, [] -> DT.Named {
+      n_info = id;
+      n_name = name;
+    }
+  | `Named "->", [p; r] -> DT.Fn {
+      fn_info = id;
+      fn_pvar = None;
+      fn_param = p;
+      fn_ret = r;
+    }
+  | `Named "<apply>", [f; x] -> DT.App {
+      app_info = id;
+      app_fn = f;
+      app_arg = x;
+    }
+  | `Named name, _ ->
+      failwith ("TODO: make_const_type " ^ name)
+  | `Extend _, _ ->
+      MuleErr.bug "kind mismatch"
+
 let get_var_type uv =
-  let var_of_int n =
-    Common_ast.Var.of_string ("$$" ^ Int.to_string n)
-  in
-  let mk_var id =
-      ( DT.Var {
-          v_info = id;
-          v_var = var_of_int id;
-        }
-      , IntSet.singleton id
-      )
-  in
-  let rec go seen uv =
-    let t = UnionFind.get uv in
-    let ty_id = get_ty_id t in
-    if Set.mem seen ty_id then
-      mk_var ty_id
-    else
-      begin match t with
-        | `Free _ -> mk_var ty_id
-        | `Quant(q_id, q, v_id, _, body) ->
-            let (body', fvs) = go (Set.add seen q_id) body in
-            maybe_add_recur
-              q_id
-              (Set.remove fvs v_id)
-              (DT.Quant {
-                  q_info = q_id;
-                  q_quant = q;
-                  q_var = var_of_int v_id;
-                  q_body = body';
-              })
-        | `Const(ty_id, `Named "{...}", [rtypes, _; rvals, _], _) ->
-            let seen' = Set.add seen ty_id in
-            let (r_types, fv_types) = go_row seen' rtypes in
-            let (r_values, fv_values) = go_row seen' rvals in
-            DT.Record {
-              r_info = ty_id;
-              r_types;
-              r_values;
-              r_src = ST.Record {r_items = []};
-            }
-            |> maybe_add_recur ty_id (Set.union fv_types fv_values)
-        | `Const(ty_id, `Named "|", [row, _], _) ->
-            let seen' = Set.add seen ty_id in
-            let (r, fvs) = go_row seen' row in
-            DT.Union{u_row = r}
-            |> maybe_add_recur ty_id fvs
-        | `Const(ty_id, c, args, _) ->
-            let seen' = Set.add seen ty_id in
-            let args_fvs =
-              List.map args ~f:(fun (t, _) -> go seen' t)
-            in
-            let args = List.map args_fvs ~f:fst in
-            let fvs =
-              List.map args_fvs ~f:snd
-              |> Set.union_list (module Int)
-            in
-            maybe_add_recur ty_id fvs (make_const_type ty_id c args)
-      end
-  and go_row seen uv =
-    let r = UnionFind.get uv in
-    let ty_id = get_ty_id r in
-    match r with
-      | `Free _ ->
-          ( (ty_id, [], Some (var_of_int ty_id))
-          , IntSet.singleton ty_id
-          )
-      | `Const(_, `Named "<empty>", _, _) ->
-          ((ty_id, [], None), IntSet.empty)
-      | `Const(_, `Extend lbl, [h, _; t, _], _) ->
-          let (h', hfv) = go seen h in
-          let ((_, fields, tail), tfv) = go_row seen t in
-          ( (ty_id, ((lbl, h') :: fields), tail)
-          , Set.union hfv tfv
-          )
-      | _ ->
-          MuleErr.bug "Invalid row"
-  and maybe_add_recur id fvs ty =
-    if Set.mem fvs id then
-      ( DT.Recur {
-          mu_info = id;
-          mu_var = var_of_int id;
-          mu_body = ty;
-        }
-      , Set.remove fvs id
-      )
-    else
-      (ty, fvs)
-  and make_const_type id c args =
-    match c, args with
-    | `Named name, [] -> DT.Named {
-        n_info = id;
-        n_name = name;
-      }
-    | `Named "->", [p; r] -> DT.Fn {
-        fn_info = id;
-        fn_pvar = None;
-        fn_param = p;
-        fn_ret = r;
-      }
-    | `Named "<apply>", [f; x] -> DT.App {
-        app_info = id;
-        app_fn = f;
-        app_arg = x;
-      }
-    | `Named name, _ ->
-        failwith ("TODO: make_const_type " ^ name)
-    | `Extend _, _ ->
-        MuleErr.bug "kind mismatch"
-  in
   fst (go IntSet.empty uv)
   |> Relabel.relabel_type ()
+
+let get_var_row uv =
+  fst (go_row (IntSet.empty) uv)
--- src/libmule/typecheck/extract.mli
+++ src/libmule/typecheck/extract.mli
@@ -2,3 +2,5 @@ open Typecheck_types
 open Desugared_ast
 
 val get_var_type: u_var -> int Type.t
+
+val get_var_row: u_var -> int Type.row
