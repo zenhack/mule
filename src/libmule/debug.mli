2ea8e5ad2ba7b8e5f6a5b50a48b2c9a1d3600344
Ian Denhardt <ian@zenhack.net>
2019-10-23 18:40:05 -0400

    Remove the "debug" module.
    Which is obsolete and unused now that the constraint based type checker
    is gone.

--- src/libmule/debug.mli
+++
@@ -1,9 +0,0 @@
-include module type of Debug_t
-
-val render_hook : (unit -> unit) ref
-
-val start_graph: unit -> unit
-val show_node: node_type -> int -> unit
-val show_edge: edge_type -> int -> int -> unit
-val end_graph: unit -> unit
-val set_root: int -> unit
