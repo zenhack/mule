77f68743e6210bb92b8e74b4c40a1f629bb7ee02
Ian Denhardt <ian@zenhack.net>
2019-11-11 16:14:35 -0500

    Util: Delete unused `fix` function.

--- src/mule/lib/util.ml
+++ src/mule/lib/util.ml
@@ -1,11 +1,4 @@
 
-
-let fix : ('a Lazy.t -> 'b) -> ('b Lazy.t -> 'a) -> ('b * 'a) =
-  fun f g ->
-  let rec a = lazy (g b)
-  and b = lazy (f a)
-  in (Lazy.force b, Lazy.force a)
-
 module IO = struct
   let slurp_file (path : string) : string Lwt.t =
     Lwt_io.with_file ~mode:Lwt_io.Input path Lwt_io.read
