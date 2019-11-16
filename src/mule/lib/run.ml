9a98b4bbc320a8ee9aed5444f16a77b1060a8de7
Ian Denhardt <ian@zenhack.net>
2019-11-16 00:25:40 -0500

    delete unused function.

--- src/mule/lib/run.ml
+++ src/mule/lib/run.ml
@@ -10,14 +10,3 @@ let run_load_result: Load.result -> unit =
          ^ " : "
          ^ Desugared_ast_type.to_string typ
         )
-
-let run : string -> unit = fun input ->
-  let path = Caml.Filename.current_dir_name ^ "/<repl>" in
-  match MParser.parse_string Parser.repl_line input path with
-  | MParser.Failed (msg, _) ->
-      Report.display_always "Parse Error" msg
-  | MParser.Success None ->
-      (* empty input *)
-      ()
-  | MParser.Success (Some expr) ->
-      run_load_result (Load.load_surface_ast ~typ:None ~expr ~extra_types:[])
