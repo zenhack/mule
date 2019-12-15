2fe66373b65f1e34d9cf0f9d850b912130c468c4
Ian Denhardt <ian@zenhack.net>
2019-12-15 12:46:15 -0500

    An attempt at getting stuff building in CI again.
    The lwt.unix package isn't something that actually gets installed via
    opam on its own; it comes with lwt.
    
    while troubleshooting this I updated ocaml on my local machine, and
    started getting a warning about Sys.argv in gen-js-runtime, so corrected
    that as well.

--- src/mule/gen-js-runtime/gen.ml
+++ src/mule/gen-js-runtime/gen.ml
@@ -1,7 +1,8 @@
 
 let () =
-  let src = Sys.argv.(1) in
-  let dest = Sys.argv.(2) in
+  let argv = Sys.get_argv () in
+  let src = argv.(1) in
+  let dest = argv.(2) in
   let src_content = Stdio.In_channel.read_all src in
   let dest_content = "let src = \"" ^ String.escaped src_content ^ "\"" in
   Stdio.Out_channel.write_all dest ~data:dest_content
