e8db5e7937c4fab89c73e3ae02a9319f08a7e225
Ian Denhardt <ian@zenhack.net>
2020-01-06 19:15:07 -0500

    Use @@deriving to kill some boilerplate

--- src/mule/lib/util/pair.ml
+++ src/mule/lib/util/pair.ml
@@ -7,28 +7,7 @@ end
 module Make(Left:Elt)(Right:Elt) = struct
   module T = struct
     type t = (Left.t * Right.t)
-
-    let sexp_of_t (l, r) =
-      Sexp.List [
-        Left.sexp_of_t l;
-        Right.sexp_of_t r;
-      ]
-
-    let t_of_sexp = function
-      | Sexp.List [l; r] ->
-          ( Left.t_of_sexp l
-          , Right.t_of_sexp r
-          )
-      | sexp ->
-          raise (Sexp.Of_sexp_error (Failure "t_of_sexp: pair needed", sexp))
-
-
-    let compare (x1, y1) (x2, y2) =
-      let r = Left.compare x1 x2 in
-      if r = 0 then
-        Right.compare y1 y2
-      else
-        r
+    [@@deriving sexp, compare]
   end
   include T
   include Comparator.Make(T)
