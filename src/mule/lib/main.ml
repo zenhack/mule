082b159f150d6ebb0c315b70a58d9ef1ce185983
Ian Denhardt <ian@zenhack.net>
2020-08-23 00:12:03 -0400

    Actually call gen_expr.

--- src/mule/lib/main.ml
+++ src/mule/lib/main.ml
@@ -11,8 +11,14 @@ let typecheck path =
   in
   Lint.check_expr exp;
   let dexp = Desugar.desugar exp in
-  let _ = dexp in
-  failwith "TODO"
+  let dexp = Desugared_ast_expr.map dexp ~f:(fun _ -> ()) in
+  let ctx = Context.make Gensym.global (fun ctx ->
+      Gen_constraints.Gen.gen_expr ctx dexp
+    )
+  in
+  let g = Context.get_g ctx in
+  let _q = Lazy.force (Graph_types.GNode.get g) in
+  failwith "TODO: do stuff after generating constraints"
 
 
 let interp_cmd = function
