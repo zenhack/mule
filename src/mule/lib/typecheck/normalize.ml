a808015549fc8af34cdb729daacf246811382d2f
Ian Denhardt <ian@zenhack.net>
2021-12-12 23:21:31 -0500

    Pass qv a bit deeper.

--- src/mule/lib/typecheck/normalize.ml
+++ src/mule/lib/typecheck/normalize.ml
@@ -1,26 +1,26 @@
 module GT = Graph_types
 
-let clone_lambda _ =
+let clone_lambda _qv (_id, _param, _body) =
   failwith "TODO: clone_lambda"
 
-let rec whnf_typ ctx t = match t with
+let rec whnf_typ ctx qv t = match t with
   | `Free _ | `Ctor _ | `Lambda _ | `Poison _ -> t
   | `Apply (id, f, arg) ->
       begin
-        Context.modify_var ctx Context.quant (whnf_q ctx) f;
-        apply_qq ctx id f arg
+        Context.modify_var ctx Context.quant (whnf_q ctx qv) f;
+        apply_qq ctx qv id f arg
       end
-and whnf_q ctx q =
-  Context.modify_var ctx Context.typ (whnf_typ ctx) (Lazy.force q.q_body);
+and whnf_q ctx qv q =
+  Context.modify_var ctx Context.typ (whnf_typ ctx qv) (Lazy.force q.q_body);
   q
-and apply_qq ctx app_id f arg =
+and apply_qq ctx qv app_id f arg =
   let ft = Lazy.force (Context.read_var ctx Context.quant f).q_body in
   match Context.read_var ctx Context.typ ft with
   | `Lambda lam ->
-    let (_id, _param, _body) = clone_lambda lam in
+    let (_id, _param, _body) = clone_lambda qv lam in
     failwith "TODO: apply_qq lambda"
   | _ ->
     `Apply (app_id, f, arg)
 
 let whnf_qv ctx qv =
-  Context.modify_var ctx Context.quant (whnf_q ctx) qv
+  Context.modify_var ctx Context.quant (whnf_q ctx qv) qv
