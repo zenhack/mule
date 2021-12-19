16f6688236cfda3267dbe07067dabe416caf3887
Ian Denhardt <ian@zenhack.net>
2021-12-18 22:21:42 -0500

    Minor correction & refactor.
    The downside of stubbing out to-be written functions with failwith is
    that singular call sites will always type check -- as this one did, even
    though we should have been extracting the `Lambda from deeper in the
    tree. Factor out the code that does this earlier into a helper function
    and do it here too.

--- src/mule/lib/typecheck/normalize.ml
+++ src/mule/lib/typecheck/normalize.ml
@@ -3,6 +3,11 @@ module GT = Graph_types
 let clone_q _seen _ctx _b_target _q =
   failwith "TODO: clone_lambda"
 
+let get_qv_typ ctx qv =
+  let q = Context.read_var ctx Context.quant qv in
+  let body = Lazy.force q.q_body in
+  Context.read_var ctx Context.typ body
+
 let rec whnf_typ ctx qv t = match t with
   | `Free _ | `Ctor _ | `Lambda _ | `Poison _ -> t
   | `Apply (id, f, arg) ->
@@ -14,12 +19,11 @@ and whnf_q ctx qv q =
   Context.modify_var ctx Context.typ (whnf_typ ctx qv) (Lazy.force q.q_body);
   q
 and apply_qq ctx qv app_id fq arg =
-  let f = Context.read_var ctx Context.quant fq in
-  let ft = Lazy.force f.q_body in
-  match Context.read_var ctx Context.typ ft with
+  match get_qv_typ ctx fq with
   | `Lambda (_, param, body) ->
     let (param, body) =
       let seen = Expand_reduce.make_seen () in
+      let f = Context.read_var ctx Context.quant fq in
       let f_bnd = Context.read_var ctx Context.bound f.q_bound in
       if Expand_reduce.bound_under seen ctx ~limit:(`Q qv) ~target:f_bnd.b_target then
         (* Already localized to this `Apply; no need to copy, as there are
@@ -27,7 +31,8 @@ and apply_qq ctx qv app_id fq arg =
         (param, body)
       else
         begin
-          match clone_q seen ctx (`Q qv) fq with
+          let fq' = clone_q seen ctx (`Q qv) fq in
+          match get_qv_typ ctx fq' with
           | `Lambda (_, param, body) -> (param, body)
           | _ -> MuleErr.bug "clone_q didn't return a lambda."
         end
